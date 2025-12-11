3dcd9f89f1bb88c4de91076319ee91da599079dc
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2025-12-10 21:39:35 -0500

    new footer

--- apps/next/components/Footer.tsx
+++ apps/next/components/Footer.tsx
@@ -7,7 +7,22 @@ import Link from "next/link";
 export default function Footer() {
   
   return (
-    <div className="w-full p-0 md:p-2 mb-8">
+    <div className="w-full p-0 my-6 mx-1 py-6">
+      <div className="flex flex-col md:flex-row p-6 gap-6 justify-around">
+        <div className="flex flex-col items-center">
+          <h3 className="font-bold">Location: Brooklyn + New York State</h3>
+          <p>Virtual Sessions Only</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <h3 className="font-bold">Hours:</h3>
+          <p>Monday-Friday</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <h3 className="font-bold">Contact:</h3>
+          <p>email@email.com</p>
+        </div>
+
+      </div>
       <footer
         className="translate-y-[100%] w-full h-8 border-[1px] px-2 py-1 flex flex-row items-center gap-4 text-xs sm:text-sm"
       >
